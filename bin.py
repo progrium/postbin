554dccc4ca2a814388ab690c576a46b495e27afe
Jeff Lindsay <progrium@gmail.com>
2009-07-05 03:06:41 -0700

    caved and made the form data display in a table. now also supporting GET requests with query params as POST requests. kinda hackish, but it should work

--- bin.py
+++ bin.py
@@ -11,22 +11,32 @@ class BinHandler(webapp.RequestHandler):
         if self.request.path[-1] == '/':
             self.redirect(self.request.path[:-1])
         bin = self._get_bin()
-        posts = bin.post_set.order('-created').fetch(50)
-        request = self.request
-        self.response.out.write(template.render('templates/bin.html', {'bin':bin, 'posts':posts, 'request':request}))
+        if self.request.query_string:
+            self._record_post(bin, True)
+            self.redirect('/%s' % bin.name)
+        else:
+            posts = bin.post_set.order('-created').fetch(50)
+            self.response.out.write(template.render('templates/bin.html', {'bin':bin, 'posts':posts, 'request':self.request}))
 
     def post(self):
         bin = self._get_bin()
-        post = Post(bin=bin, remote_addr=self.request.remote_addr)
-        post.headers        = dict(self.request.headers)
-        post.body           = self.request.body
-        post.query_string   = self.request.query_string
-        post.form_data      = [[k,v] for k,v in self.request.POST.items()]
-        post.put()
+        self._record_post(bin)
         if 'http://' in self.request.query_string:
             urlfetch.fetch(url=self.request.query_string.replace('http://', 'http://hookah.webhooks.org/'),
                             payload=urllib.urlencode(self.request.POST.items()), method='POST')
         self.redirect('/%s' % bin.name)
+
+    def _record_post(self, bin, use_get=False):
+        post = Post(bin=bin, remote_addr=self.request.remote_addr)
+        post.headers        = dict(self.request.headers)
+        post.body           = self.request.body
+        post.query_string   = self.request.query_string
+        if use_get:
+            data_source = self.request.GET
+        else:
+            data_source = self.request.POST
+        post.form_data      = [[k,v] for k,v in data_source.items()]
+        post.put()
 
     def _get_bin(self):
         name = self.request.path.replace('/', '')
